cc0825d04680fb4e4b956647accbc1dc2c814c84
Jannik Semperowitsch <jannik.semperowitsch@knime.com>
2026-05-06 12:13:30 +0200

    SLEEK-160: SLEEK-160: WebUI Migration of RDKit Add Hs node
    SLEEK-160 ()

--- org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/addhs/RDKitAddHsNodeFactory.java
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/addhs/RDKitAddHsNodeFactory.java
@@ -51,15 +51,27 @@ package org.rdkit.knime.nodes.addhs;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.node.NodeDescription;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import org.knime.node.impl.description.PortDescription;
+import java.util.List;
+import static org.knime.node.impl.description.PortDescription.fixedPort;
 
 /**
  * <code>NodeFactory</code> for the RDKit based "RDKitAddHs" Node.
  * Adds hydrogens to an RDKit molecule.
  *
  * @author Manuel Schwarze
+ * @author Jannik Semperowitsch, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
  */
 public class RDKitAddHsNodeFactory 
-        extends NodeFactory<RDKitAddHsNodeModel> {
+        extends NodeFactory<RDKitAddHsNodeModel> implements NodeDialogFactory {
 
     /**
      * Creates a model for the RDKitAddHs functionality
@@ -110,9 +122,52 @@ public class RDKitAddHsNodeFactory
     /**
      * {@inheritDoc}
      */
+    private static final String NODE_NAME = "RDKit Add Hs";
+    private static final String NODE_ICON = "default.png";
+    private static final String SHORT_DESCRIPTION = """
+            Adds hydrogens to an RDKit molecule.
+            """;
+    private static final String FULL_DESCRIPTION = """
+            Adds hydrogens to an RDKit molecule.
+            """;
+    private static final List<PortDescription> INPUT_PORTS = List.of(
+            fixedPort("Input table with RDKit Molecules", """
+                Input table with RDKit Molecules
+                """)
+    );
+    private static final List<PortDescription> OUTPUT_PORTS = List.of(
+            fixedPort("Output table with RDKit Molecules with added Hs", """
+                Output table with RDKit Molecules with added Hs
+                """)
+    );
+
     @Override
     public NodeDialogPane createNodeDialogPane() {
-        return new RDKitAddHsNodeDialog();
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
     }
+
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, RDKitAddHsNodeParameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription( //
+            NODE_NAME, //
+            NODE_ICON, //
+            INPUT_PORTS, //
+            OUTPUT_PORTS, //
+            SHORT_DESCRIPTION, //
+            FULL_DESCRIPTION, //
+            List.of(), //
+            RDKitAddHsNodeParameters.class, //
+            null, //
+            NodeType.Manipulator, //
+            List.of(), //
+            null //
+        );
+    }
+    
 }
 
