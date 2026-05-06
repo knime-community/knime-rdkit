c27fb0df0146146a181d702d162c92c77b75c045
Jannik Semperowitsch <jannik.semperowitsch@knime.com>
2026-05-06 11:26:31 +0200

    SLEEK-114: created RDKitMoleculeColumnChoicesProvider
    SLEEK-114 (Create shared classes and auxiliary functions)

---
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/util/RDKitMoleculeColumnChoicesProvider.java
@@ -0,0 +1,60 @@
+package org.rdkit.knime.util;
+
+import java.util.List;
+
+import org.knime.chem.types.SmartsValue;
+import org.knime.chem.types.SmilesValue;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.DataValue;
+import org.knime.node.parameters.NodeParametersInput;
+import org.knime.node.parameters.widget.choices.ColumnChoicesProvider;
+import org.rdkit.knime.types.RDKitMolValue;
+
+public class RDKitMoleculeColumnChoicesProvider implements ColumnChoicesProvider {
+    private int inputTableIndex = 0;
+    private final Class<? extends DataValue>[] valueClasses;
+    
+    // Default RDKit types used if none are provided
+    @SuppressWarnings("unchecked")
+	private static final Class<? extends DataValue>[] DEFAULT_CLASSES = new Class[] { 
+        RDKitMolValue.class, 
+        SmartsValue.class, 
+        SmilesValue.class 
+    };
+
+    @Override
+    public List<DataColumnSpec> columnChoices(final NodeParametersInput context) {
+        return context
+                .getInTableSpec(inputTableIndex).map(spec -> spec.stream()
+                        .filter(this::isColumnCompatible).toList())
+                .orElse(List.of());
+    }
+
+    // 1- Empty constructor
+    public RDKitMoleculeColumnChoicesProvider() {
+        this(0, DEFAULT_CLASSES);
+    }
+
+    // 2- Constructor with custom inputTableIndex
+    public RDKitMoleculeColumnChoicesProvider(int inputTableIndex) {
+        this(inputTableIndex, DEFAULT_CLASSES);
+    }
+
+    // 3- Constructor with custom index and specific value classes
+    public RDKitMoleculeColumnChoicesProvider(int inputTableIndex, Class<? extends DataValue>[] valueClasses) {
+        this.inputTableIndex = inputTableIndex;
+        this.valueClasses = valueClasses;
+    }
+
+    @SuppressWarnings("unused")
+	private boolean isColumnCompatible(DataColumnSpec colSpec) {
+        DataType colType = colSpec.getType();
+        for (Class<? extends DataValue> clazz : valueClasses) {
+            if (colType.isCompatible(clazz) || colType.isAdaptable(clazz)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
