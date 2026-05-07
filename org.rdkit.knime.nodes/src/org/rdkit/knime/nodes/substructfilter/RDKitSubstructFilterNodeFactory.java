b13e293b03f7f599a5ddca3c2f10d1b19879af86
Magnus Gohm <magnus.gohm@knime.com>
2026-05-07 16:21:08 +0200

    SLEEK-185: WebUI Migration of RDKit Substructure Filter
    SLEEK-185 (WebUI-Migration RDKit Substructure Filter)

--- org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/substructfilter/RDKitSubstructFilterNodeFactory.java
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/substructfilter/RDKitSubstructFilterNodeFactory.java
@@ -51,15 +51,57 @@ package org.rdkit.knime.nodes.substructfilter;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.node.NodeDescription;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import org.knime.node.impl.description.PortDescription;
+import java.util.List;
+import static org.knime.node.impl.description.PortDescription.fixedPort;
 
 /**
  * <code>NodeFactory</code> for the RDKit based "RDKitSubstructFilter" Node.
  * 
  * @author Greg Landrum
  * @author Manuel Schwarze
+ * @author Magnus Gohm, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
  */
-public class RDKitSubstructFilterNodeFactory extends NodeFactory<RDKitSubstructFilterNodeModel> {
+public class RDKitSubstructFilterNodeFactory extends NodeFactory<RDKitSubstructFilterNodeModel> 
+	implements NodeDialogFactory {
 
+    private static final String NODE_NAME = "RDKit Substructure Filter";
+    
+    private static final String NODE_ICON = "default.png";
+    
+    private static final String SHORT_DESCRIPTION = """
+            Applies a substructure filter to an input RDKit Mol column.
+            """;
+    
+    private static final String FULL_DESCRIPTION = """
+            Applies a substructure filter to an input RDKit Mol column. The substructure query is given as a
+            single SMARTS pattern. Molecules that match the pattern appear in the first output table, molecules
+            that don't match appear in the second output table.
+            """;
+    
+    private static final List<PortDescription> INPUT_PORTS = List.of(
+            fixedPort("Molecules", """
+                Table with RDKit molecules.
+                """)
+    );
+    
+    private static final List<PortDescription> OUTPUT_PORTS = List.of(
+            fixedPort("Molecules matching filter", """
+                Molecules matching the substructure query.
+                """),
+            fixedPort("Molecules not matching filter", """
+                Molecules not matching the substructure query.
+                """)
+    );
+	
 	/**
 	 * Creates a model for the RDKitSubstructureFilter functionality
 	 * of the RDKit library. The model is derived from the
@@ -106,12 +148,33 @@ public class RDKitSubstructFilterNodeFactory extends NodeFactory<RDKitSubstructF
 		return true;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public NodeDialogPane createNodeDialogPane() {
-		return new RDKitSubstructFilterNodeDialog();
-	}
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
+    }
+
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, RDKitSubstructFilterNodeParameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription( //
+            NODE_NAME, //
+            NODE_ICON, //
+            INPUT_PORTS, //
+            OUTPUT_PORTS, //
+            SHORT_DESCRIPTION, //
+            FULL_DESCRIPTION, //
+            List.of(), //
+            RDKitSubstructFilterNodeParameters.class, //
+            null, //
+            NodeType.Manipulator, //
+            List.of(), //
+            null //
+        );
+    }
+    
 }
 
