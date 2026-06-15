e61d5c5153c58dbefccd8db03b815c9fe9dc0226
jannik-sem <jannik.semperowitsch@knime.com>
2026-06-15 14:04:47 +0200

    SLEEK-172: WebUI Migration of RDKit From IUPAC (#10)
    SLEEK-172 (WebUI-Migration RDKit From IUPAC)

---
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/iupac2rdkit/RDKitIUPACToRDKitNodeParameters.java
@@ -0,0 +1,126 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ */
+
+package org.rdkit.knime.nodes.iupac2rdkit;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.StringValue;
+import org.knime.node.parameters.NodeParameters;
+import org.knime.node.parameters.NodeParametersInput;
+import org.knime.node.parameters.Widget;
+import org.knime.node.parameters.migration.LoadDefaultsForAbsentFields;
+import org.knime.node.parameters.persistence.Persist;
+import org.knime.node.parameters.updates.ParameterReference;
+import org.knime.node.parameters.updates.ValueProvider;
+import org.knime.node.parameters.updates.ValueReference;
+import org.knime.node.parameters.legacy.updates.ColumnNameAutoGuessValueProvider;
+import org.knime.node.parameters.widget.choices.ChoicesProvider;
+import org.knime.node.parameters.widget.choices.util.ColumnSelectionUtil;
+import org.knime.node.parameters.widget.choices.util.CompatibleColumnsProvider;
+import org.rdkit.knime.util.RDKitAdapterCellSupport;
+import org.rdkit.knime.util.RDKitResultColumnNameAutoGuessProvider;
+
+/**
+ * Node parameters for RDKit From IUPAC.
+ *
+ * @author Jannik Semperowitsch, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
+ */
+@LoadDefaultsForAbsentFields
+final class RDKitIUPACToRDKitNodeParameters implements NodeParameters {
+
+    @Widget(title = "IUPAC name column", description = "The input column with IUPAC names.")
+    @ChoicesProvider(InputColumnChoicesProvider.class)
+    @ValueProvider(InputColumnAutoGuessProvider.class)
+    @ValueReference(InputColumnRef.class)
+    @Persist(configKey = RDKitIUPACToRDKitNodeModel.CFG_INPUT_COLUMN)
+    String m_inputColumn;
+
+    interface InputColumnRef extends ParameterReference<String> {
+    }
+    
+    static final class InputColumnChoicesProvider extends CompatibleColumnsProvider {
+
+		protected InputColumnChoicesProvider() {
+			super(Arrays.asList(RDKitAdapterCellSupport.expandByAdaptableTypes(StringValue.class)));
+		}
+    	
+    }
+
+    static final class InputColumnAutoGuessProvider extends ColumnNameAutoGuessValueProvider {
+
+        InputColumnAutoGuessProvider() {
+            super(InputColumnRef.class);
+        }
+
+        @Override
+        protected Optional<DataColumnSpec> autoGuessColumn(final NodeParametersInput parametersInput) {
+            return ColumnSelectionUtil.getFirstCompatibleColumn(parametersInput, 0,
+                RDKitAdapterCellSupport.expandByAdaptableTypes(StringValue.class));
+        }
+    }
+
+    @Widget(title = "New column name",
+        description = "The name of the new column, which will contain the calculation results.")
+    @Persist(configKey = RDKitIUPACToRDKitNodeModel.CFG_NEW_COLUMN_NAME)
+    @ValueProvider(NewColumnNameProvider.class)
+    @ValueReference(NewColumnNameRef.class)
+    String m_newColumnName;
+    
+    static final class NewColumnNameRef implements ParameterReference<String> {
+	}
+    
+    static final class NewColumnNameProvider extends RDKitResultColumnNameAutoGuessProvider {
+
+		protected NewColumnNameProvider() {
+			super(InputColumnRef.class, NewColumnNameRef.class, "(RDKit Mol)");
+		}
+    	
+    }
+}
